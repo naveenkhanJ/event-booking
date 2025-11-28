37a6d9243e809284443d0d4b3ebd94bbcf06e6e9
naveenkhanJ <it23812826@my.sliit.lk>
2025-11-28 21:28:50 +0530

    Fix useEffect import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ScheduleTable from './components/ScheduleTable';
 import BookingModal from './components/BookingModal';
 import DateTabs from './components/DateTabs';
